7ae55a27480833ce85f31078502408b0f41ceeea
Pea Lutz <pea@glocal.coop>
2016-04-03 22:53:21 -0400

    Added `get_post` test

--- tests/test-cpt.php
+++ tests/test-cpt.php
@@ -42,4 +42,27 @@ class CustomPostTypeTests extends WP_UnitTestCase {
         $this->assertTrue( $user_id == $post->post_author );
         $this->assertTrue( $post_type == $post->post_type );
     }
+
+    /**
+     * Test to see if `get_post` works.
+     *
+     * Compares a post ID (int) `$org_post_id` with post ID 
+     * taken out of get_post `$new_post_id`.
+     * If they don't match, `get_post` doesn't work, and it will 
+     * return an error.
+     */
+    public function test_kb_get_post() {
+        //Create new post using method provided by WP
+        $org_post_id = $this->factory->post->create();
+
+        //Get post object using the new post's ID
+        $post_obj = get_post( $org_post_id );
+     
+        //Get the post ID as given to us by get_post
+        $new_post_id = $post_obj->ID;
+     
+        //Use pre-defined method to test if the two IDs match
+        $this->assertEquals( $org_post_id, $new_post_id );
+             
+    }
 }
