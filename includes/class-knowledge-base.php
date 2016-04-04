0cf9d86f94e2c8fa88e81c86ce2bbecb57f56f48
Pea Lutz <pea@glocal.coop>
2016-04-03 22:52:31 -0400

    Added filters Switched from explicit capabilities to `$capability_type = 'post'`

--- includes/class-knowledge-base.php
+++ includes/class-knowledge-base.php
@@ -30,49 +30,61 @@ class Knowledge_Base_Entry {
      * Capabilities needed to act on the custom post type.
      *
      * @var array
+     *
+     * @link https://codex.wordpress.org/Roles_and_Capabilities
+     *
+     * Added `delete_posts` to capabilities in order to fix error
+     * `Notice: Undefined property: stdClass::$delete_posts /wp-admin/includes/class-wp-posts-list-table.php on line 403`
+     *
+     * @link https://wordpress.org/support/topic/custom-post-types-do-not-have-move-to-trash-bulk-action#post-6884627
      */
-    private $capabilities = array(
-        'edit_post'          => 'edit_posts',
-        'read_post'          => 'edit_posts',
-        'delete_post'        => 'edit_posts',
-        'edit_posts'         => 'edit_posts',
-        'edit_others_posts'  => 'edit_posts',
-        'publish_posts'      => 'edit_posts',
-        'read_private_posts' => 'edit_posts',
+    private $capability_type = 'post';
+
+    /**
+     * Registers support of certain feature(s) for this post type
+     *
+     * @link https://codex.wordpress.org/Function_Reference/register_post_type#supports
+     */
+    private $supports = array(
+        'title',
+        'editor',
+        'revisions',
+        'excerpt',
+        'thumbnail',
+        'page-attributes'
     );
 
     /**
      * Constructor.
      */
     public function __construct () {
-        $this->labels = array(
+        $this->labels = apply_filters( 'kb_post_type_labels', array(
             'name'                  => _x( 'Knowledge Base', 'Post Type General Name', Knowledge_Base::$text_domain ),
             'singular_name'         => _x( 'Knowledge Base Item', 'Post Type Singular Name', Knowledge_Base::$text_domain ),
-        );
+        ) );
     }
 
     /**
      * Registers the custom post type with via WordPress API.
      */
-    public function register () {
-        register_post_type( self::name, array(
-            'labels'       => apply_filters( 'kb_labels', $this->labels ),
-            'public'       => true,
-            'show_in_menu' => true,
-            'hierarchical' => apply_filters( 'kb_hierarchical', false ),
-            'has_archive'  => true,
-            'capabilities' => apply_filters( 'kb_capabilities', $this->capabilities ),
-            'supports'     => array(
-                'title',
-                'editor',
-                'revisions',
-                'excerpt',
-                'thumbnail',
-                'page-attributes'
-            ),
-            'menu_icon'    => apply_filters( 'kb_menu_icon', 'dashicons-lightbulb' ),
-            'taxonomies'   => array( Knowledge_Base_Taxonomy::name ),
-        ) );
+    public function register() {
+        register_post_type(
+            self::name,
+            apply_filters( 'kb_register_custom_post_type', array(
+                'labels'              => $this->labels,
+                'supports'            => $this->supports,
+                'capability_type'     => $this->capability_type,
+                'public'              => true,
+                'has_archive'         => true,
+                'hierarchical'        => false,
+                'menu_icon'           => 'dashicons-lightbulb',
+                'show_in_rest'        => true,
+                'rest_base'           => self::name,
+                'rest_controller_class' => 'WP_REST_Posts_Controller',
+                'taxonomies'          => array( Knowledge_Base_Taxonomy::name ),
+            ) )
+        );
+
     }
 
     /**
@@ -82,15 +94,12 @@ class Knowledge_Base_Entry {
      *
      * @return array
      */
-    public function get_posts ( $args = null ) {
+    public function get_posts( $args = null ) {
         if ( !is_null( $args ) ) {
             $args = wp_parse_args( $args, array(
                 'post_type' => self::name
             ) );
         }
-        // TODO: We should consider making this a variable so the end
-        //       user can determine which blog to save the site-wide
-        //       directory metadata to.
         $posts = get_posts( $args );
         return $posts;
     }
